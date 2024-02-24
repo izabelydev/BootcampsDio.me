0abaa08b83310d06b6c90f3f5395383185743c23
izabelydev <izabelylrnc@gmail.com>
2024-02-24 17:36:56 -0300

    Aulas sobre ActiveRouter no Angular

---
+++ Desenvolvimento Frontend com Angular/Introdução ao Angular Framework/rotas-angular/rotas/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portifolio/card/card.component';
+
+const routes: Routes = [
+  {path:'', component: TitleComponent, pathMatch: 'full'},
+  {path: 'portfolio', component: CardComponent},
+  {path:'**', redirectTo:''}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
